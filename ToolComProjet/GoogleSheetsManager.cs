38e004ca0d87887eb0bfeb5be1f65901b0aab717
Tssoki <th.boi22@hotmail.com>
2024-05-06 16:47:00 +0200

    init

---
+++ ToolComProjet/GoogleSheetsManager.cs
@@ -0,0 +1,115 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using System;
+using System.Collections.Generic;
+
+namespace ToolComProjet
+{
+    public class GoogleSheetsManager : IGoogleSheetsManager
+    {
+        private readonly UserCredential credential;
+
+        public GoogleSheetsManager(UserCredential _credential)
+        {
+            credential = _credential;
+        }
+
+        public Spreadsheet CreateNew(string _documentName)
+        {
+            if (string.IsNullOrEmpty(_documentName))
+                throw new ArgumentNullException(nameof(_documentName));
+
+            using (var _sheetsService = new SheetsService(new BaseClientService.Initializer() { HttpClientInitializer = credential }))
+            {
+                var _documentCreationRequest = _sheetsService.Spreadsheets.Create(new Spreadsheet()
+                {
+                    Sheets = new List<Sheet>()
+                    {
+                        new Sheet()
+                        {
+                            Properties = new SheetProperties()
+                            {
+                                Title = _documentName
+                            }
+                        }
+                    },
+
+                    Properties = new SpreadsheetProperties()
+                    {
+                        Title = _documentName
+                    }
+                }); ;
+
+                return _documentCreationRequest.Execute();
+            }
+        }
+
+        public Spreadsheet GetSpreadSheet(string _googleSpreadsheetIdentifier)
+        {
+            if (string.IsNullOrEmpty(_googleSpreadsheetIdentifier))
+                throw new ArgumentNullException(nameof(_googleSpreadsheetIdentifier));
+
+            using (var _sheetService = new SheetsService(new BaseClientService.Initializer() { HttpClientInitializer = credential }))
+                return _sheetService.Spreadsheets.Get(_googleSpreadsheetIdentifier).Execute();
+        }
+
+        public ValueRange GetSingleValue(string _googleSpreadsheetIdentifier, string _valueRange)
+        {
+            if (string.IsNullOrEmpty(_googleSpreadsheetIdentifier))
+                throw new ArgumentNullException(nameof(_googleSpreadsheetIdentifier));
+            if (string.IsNullOrEmpty(_valueRange))
+                throw new ArgumentNullException(nameof(_valueRange));
+
+            using (var _sheetService = new SheetsService(new BaseClientService.Initializer() { HttpClientInitializer = credential }))
+            {
+                var _getValueRequest = _sheetService.Spreadsheets.Values.Get(_googleSpreadsheetIdentifier, _valueRange);
+                return _getValueRequest.Execute();
+            }
+        }
+
+        public void RemoveSingleValue(string _googleSpreadsheetIdentifier, string _valueRange)
+        {
+            if (string.IsNullOrEmpty(_googleSpreadsheetIdentifier))
+                throw new ArgumentNullException(nameof(_googleSpreadsheetIdentifier));
+            if (string.IsNullOrEmpty(_valueRange))
+                throw new ArgumentNullException(nameof(_valueRange));
+
+            using (var _sheetService = new SheetsService(new BaseClientService.Initializer() { HttpClientInitializer = credential }))
+            {
+                var _removeValueRequest = _sheetService.Spreadsheets.Values.Clear(new ClearValuesRequest(), _googleSpreadsheetIdentifier, _valueRange);
+                _removeValueRequest.Execute();
+            }
+        }
+
+        public BatchGetValuesResponse GetMultipleValues(string _googleSpreadsheetIdentifier, string[] _ranges)
+        {
+            if (string.IsNullOrEmpty(_googleSpreadsheetIdentifier))
+                throw new ArgumentNullException(nameof(_googleSpreadsheetIdentifier));
+            if (_ranges == null || _ranges.Length == 0)
+                throw new ArgumentNullException(nameof(_ranges));
+
+            using (var _sheetService = new SheetsService(new BaseClientService.Initializer() { HttpClientInitializer = credential }))
+            {
+                var _getValueRequest = _sheetService.Spreadsheets.Values.BatchGet(_googleSpreadsheetIdentifier);
+                _getValueRequest.Ranges = _ranges;
+                return _getValueRequest.Execute();
+            }
+        }
+
+        public void ClearMultipleValues(string _googleSpreadsheetIdentifier, string[] _ranges)
+        {
+            if (string.IsNullOrEmpty(_googleSpreadsheetIdentifier))
+                throw new ArgumentNullException(nameof(_googleSpreadsheetIdentifier));
+            if (_ranges == null || _ranges.Length == 0)
+                throw new ArgumentNullException(nameof(_ranges));
+
+            using (var _sheetService = new SheetsService(new BaseClientService.Initializer() { HttpClientInitializer = credential }))
+            {
+                var _getValueRequest = _sheetService.Spreadsheets.Values.BatchClear(new BatchClearValuesRequest() { Ranges = _ranges }, _googleSpreadsheetIdentifier);
+                _getValueRequest.Execute();
+            }
+        }
+    }
+}
---
+++ ToolComProjet/Program.cs
@@ -0,0 +1,44 @@
+﻿using Google.Apis.Auth.OAuth2;
+using System;
+using System.Linq;
+
+namespace ToolComProjet
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string _googleClientID = "783201556069-qvrac98c53djpeede70t42pvopjqvs3f.apps.googleusercontent.com";
+            string _googleClientSecret = "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";
+            string[] _scopes = new[] { Google.Apis.Sheets.v4.SheetsService.Scope.Spreadsheets };
+
+            UserCredential _credential = GoogleAuthentification.Login(_googleClientID, _googleClientSecret, _scopes);
+            GoogleSheetsManager manager = new GoogleSheetsManager(_credential);
+
+            var _mySpreadSheetID = "199FDAfzCOxDYywfilvP5fopvGnp7_YrUv4VAlGVCXqA";
+            var _spreadSheet = manager.GetSpreadSheet(_mySpreadSheetID);
+
+            //var _finalAmountOfMoney = manager.GetSingleValue(_spreadSheet.SpreadsheetId, "B2");        // SINGLE VALUE
+            //Console.WriteLine("Final amount of money: " + _finalAmountOfMoney.Values.First().First());
+
+            string _nameRange = "A2:A7";
+            string _nbrRange = "B2:B7";
+
+            string[] _valueRange = new[] { _nameRange, _nbrRange };
+            var _multipleResponse = manager.GetMultipleValues(_mySpreadSheetID, _valueRange);
+
+            var _nameResponse = _multipleResponse.ValueRanges.ElementAt(0);
+            var _nbrResponse = _multipleResponse.ValueRanges.ElementAt(1);
+
+            Console.WriteLine("\nPrinting data:");
+            for (int i = 0; i < _nameResponse.Values.Count; i++)
+            {
+                var _name = _nameResponse.Values[i].First();
+                var _nbr = _nbrResponse.Values[i].First();
+
+                Console.WriteLine("Name: " + _name + "\tNBR: " + _nbr);
+            }
+            Console.ReadLine();
+        }
+    }
+}
